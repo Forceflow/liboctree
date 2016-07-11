cc339438a0fab40d50bfb77265826c3f086337f6
Jeroen Baert <jerbae@gmail.com>
2016-07-11 18:49:54 +0200

    Simplifying octree file format

--- include/octree_io.h
+++ include/octree_io.h
@@ -1,9 +1,9 @@
 #ifndef OCTREE_IO_H_
 #define OCTREE_IO_H_
 
-#include <cstdio>
+#include <iostream>
 #include <tuple>
-#include <cstring>
+#include <string>
 #include <vector>
 #include <fstream>
 #include "OctreeNode.h"
@@ -15,9 +15,6 @@ typedef tuple<string, unsigned int, unsigned int> field_descriptor; // descripto
 
 // Internal format to interact with an octree file and its associates
 struct OctreeFile {
-	int version;
-	bool valid;
-
 	// Base filename
 	string base_filename;
 
@@ -67,49 +64,74 @@ struct OctreeFile {
 		file_nodes.write((char*) node, size_node);
 		return writepos;
 	}
-
-	OctreeFile(char* filepath) {
-
-	}
 };
 
-// Write an octree header to a file
-inline void writeOctreeHeader(const std::string &filename, const OctreeInfo &i){
-	ofstream outfile;
-	outfile.open(filename.c_str(), ios::out);
-	outfile << "#octreeheader 1" << endl;
-	outfile << "gridlength " << i.gridlength << endl;
-	outfile << "n_nodes " << i.n_nodes << endl;
-	outfile << "n_data " << i.n_data << endl;
-	outfile << "END" << endl;
-	outfile.close();
-}
-
-// Parse a given octree header, store info in OctreeInfo struct
-inline int parseOctreeHeader(const std::string &filename, OctreeInfo &i){
-	cout << "  reading octree header from " << filename << " ... " << endl;
+inline OctreeFile readOctreeFile(const string filename) {
+	std::cout << "  reading octree header from " << filename.c_str() << " ... " << endl;
 	ifstream headerfile;
-	headerfile.open(filename.c_str(), ios::in);
+	headerfile.open(filename.c_str());
 
-	i.base_filename = filename.substr(0,filename.find_last_of("."));
+	OctreeFile octreefile;
+	octreefile.base_filename = filename.substr(0, filename.find_last_of("."));
 
-	string line; bool done = false;
-	headerfile >> line >> i.version;
-	if (line.compare("#octreeheader") != 0) {cout << "    Error: first line reads [" << line << "] instead of [#octreeheader]" << endl; return 0;}
+	std::string line; 
+	bool done = false;
 
-	while(headerfile.good() && !done) {
-		headerfile >> line;
-		if (line.compare("END") == 0) done = true; // when we encounter data keyword, we're at the end of the ASCII header
-		else if (line.compare("gridlength") == 0) {headerfile >> i.gridlength;}
-		else if (line.compare("n_nodes") == 0) {headerfile >> i.n_nodes;}
-		else if (line.compare("n_data") == 0) {headerfile >> i.n_data;}
-		else { cout << "  unrecognized keyword [" << line << "], skipping" << endl;
-		char c; do { c = headerfile.get(); } while(headerfile.good() && (c != '\n'));
-		}
-	}
+	headerfile >> line;
+	if (line.compare("#octree") != 0) { cout << "    Error: first line reads [" << line.c_str() << "] instead of [#octree]" << endl; exit(1); }
+	octreefile.version = (headerfile.get());
 
-	headerfile.close();
-	return 1;
+
+	//while (headerfile.good() && !done) {
+	//	headerfile >> line;
+	//	if (line.compare("END") == 0) done = true; // when we encounter data keyword, we're at the end of the ASCII header
+	//	else if (line.compare("gridlength") == 0) { headerfile >> i.gridlength; }
+	//	else if (line.compare("n_nodes") == 0) { headerfile >> i.n_nodes; }
+	//	else if (line.compare("n_data") == 0) { headerfile >> i.n_data; }
+	//	else {
+	//		cout << "  unrecognized keyword [" << line << "], skipping" << endl;
+	//		char c; do { c = headerfile.get(); } while (headerfile.good() && (c != '\n'));
+	//	}
+	//}
 }
+
+//// Write an octree header to a file
+//inline void writeOctreeHeader(const std::string &filename, const OctreeInfo &i){
+//	ofstream outfile;
+//	outfile.open(filename.c_str(), ios::out);
+//	outfile << "#octreeheader 1" << endl;
+//	outfile << "gridlength " << i.gridlength << endl;
+//	outfile << "n_nodes " << i.n_nodes << endl;
+//	outfile << "n_data " << i.n_data << endl;
+//	outfile << "END" << endl;
+//	outfile.close();
+//}
+//
+//// Parse a given octree header, store info in OctreeInfo struct
+//inline int parseOctreeHeader(const std::string &filename, OctreeInfo &i){
+//	cout << "  reading octree header from " << filename << " ... " << endl;
+//	ifstream headerfile;
+//	headerfile.open(filename.c_str(), ios::in);
+//
+//	i.base_filename = filename.substr(0,filename.find_last_of("."));
+//
+//	string line; bool done = false;
+//	headerfile >> line >> i.version;
+//	if (line.compare("#octreeheader") != 0) {cout << "    Error: first line reads [" << line << "] instead of [#octreeheader]" << endl; return 0;}
+//
+//	while(headerfile.good() && !done) {
+//		headerfile >> line;
+//		if (line.compare("END") == 0) done = true; // when we encounter data keyword, we're at the end of the ASCII header
+//		else if (line.compare("gridlength") == 0) {headerfile >> i.gridlength;}
+//		else if (line.compare("n_nodes") == 0) {headerfile >> i.n_nodes;}
+//		else if (line.compare("n_data") == 0) {headerfile >> i.n_data;}
+//		else { cout << "  unrecognized keyword [" << line << "], skipping" << endl;
+//		char c; do { c = headerfile.get(); } while(headerfile.good() && (c != '\n'));
+//		}
+//	}
+//
+//	headerfile.close();
+//	return 1;
+//}
 
 #endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,14 +1,9 @@
 #include "../include/OctreeNode.h"
+#include "../include/octree_io.h"
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	OctreeNode n;
-	n.data = 0;
-	n.children_base = 2;
-	n.children = (byte) 146;
-	for (int i = 0; i < 8; i++) {
-		cout << "Child " << i << " Is there: " << n.hasChild(i) << " Location: " << n.getChildPos(i) << endl;
-	}
+	readOctreeFile("D:/test.octree");
 }
